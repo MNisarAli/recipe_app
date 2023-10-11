e5b1659f2c95e24b193dd31a69a0ac5055decafd
anita00001 <sharma.anita00001@gmail.com>
2023-10-11 12:17:52 +0545

    Add validation: name and description of recipe should be present

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,7 @@
 class Recipe < ApplicationRecord
 #   has_many : details
   belongs_to :user, class_name: 'User', foreign_key: 'author_id'
+
+  validates :name, presence: true, length: { maximum: 100 }
+  validates : description, presence: true, length: { maximum: 1000 }
 end
