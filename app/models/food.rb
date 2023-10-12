c5df9f36c9051707a06161ccc5a172080a5b990c
Anita Sharma <sharma.anita00001@gmail.com>
2023-10-12 10:10:41 +0545

    Add name, price and measurement_unit in food index page

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,5 +3,6 @@ class Food < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 100 }
   validates :price, presence: true
+  validates :measurement_unit, presence: true
   validates :quantity, presence: true
 end
