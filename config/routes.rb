8815c3bed865084964413c3a7f85086b25f2feb2
Anita Sharma <sharma.anita00001@gmail.com>
2023-10-12 15:01:04 +0545

    Render Navigation Menu

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/") to the public_recipes controller's index action
   root "users#index"
-
+  get '/foods', to: 'foods#index'
+  get '/recipes', to: 'recipes#index'
   get '/public_recipes', to: 'public_recipes#index'
+  
   delete '/recipes/:id', to: 'recipes#destroy', as: 'destroy_recipe'
   delete '/foods/:id', to: 'foods#destroy', as: 'destroy_food'
 end
